e2be859810772184f410464249035508b1a92162
Tony-Ismail <duchemin.tony.dev@gmail.com>
2022-08-01 14:06:51 +0200

    Router + Quiz

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Survey from './pages/Survey'
 import Header from './components/Header'
+import Error from './components/Error'
+import Results from './pages/Results'
+import Freelances from './pages/Freelances'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
@@ -12,6 +15,9 @@ root.render(
     <Routes>
       <Route exact path='/' element={<Home />}></Route>
       <Route path='/survey/:questionNumber' element={<Survey />}></Route>
+      <Route path='/results' element={<Results />}></Route>
+      <Route path='/freelances' element={<Freelances />}></Route>
+      <Route path='*' element={<Error />}></Route>
     </Routes>
   </BrowserRouter>,
 )
---
+++ src/pages/Results.jsx
@@ -0,0 +1,5 @@
+function Results() {
+  return <h1>Resultats</h1>
+}
+
+export default Results
